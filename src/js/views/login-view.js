b0be4df794e95c0223107b91b127f09bfaa54908
Tyler Oswald <oswaldtd7@gmail.com>
2015-07-31 08:47:32 -0500

    thur work

--- src/js/views/login-view.js
+++ src/js/views/login-view.js
@@ -1,12 +1,52 @@
+import User from '../models/user';
+import Router from '../routers/router';
+
 var LoginView = Backbone.View.extend({
   template: _.template($('#loginView').html()),
+
+  events: {
+    'click button': 'onSubmit'
+  },
+
+  onSubmit: function() {
+    var $loginEmail = this.$('.loginEmailInput');
+    var $loginPassword = this.$('.loginPswdInput');
+    // var $email = this.$('.emailInput');
+    // var $password = this.$('.passwordInput');
+
+    if ($loginEmailInput.val() === $email.val()
+      && $loginPswdInput.val() === $passwordInput.val()) {
+      var user = User({
+        email: $email.val(),
+        password: $password.val()
+      });
+
+      user.save();
+      Router.navigate('feed', {trigger: true});
+
+    } else {
+      alert('Error: Username or Password.');
+    }
+
+  },
 
   render: function() {
     this.$el.html(this.template());
     return this;
-  },
-
+  }
 
 });
 
 export default LoginView;
+
+
+//   success: function() {
+//     if ()
+//   },
+//
+//   error: function() {
+//     if ()
+//   }
+// });
+
+// Save the user to the server via the api
---
+++ src/js/views/newTweet-view.js
@@ -0,0 +1,12 @@
+var NewTweet = Backbone.View.extend({
+  template: _.template($('#newTweet').html()),
+
+  render: function() {
+    this.$el.html(this.template());
+    return this;
+  },
+
+
+});
+
+export default NewTweet;
