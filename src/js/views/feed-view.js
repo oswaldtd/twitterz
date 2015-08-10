d3a03f2b38d0c1443ed8e40b00e4b20af52d9b66
Adam Williams <awilli1186@gmail.com>
2015-08-10 08:58:05 -0500

    updated user view

--- src/js/views/feed-view.js
+++ src/js/views/feed-view.js
@@ -8,12 +8,12 @@ var html = `
   <h2>Tweets</h2>
   <a href="#/feed/new"> <i class="fa fa-plus"></i> New Tweet</a>
   <a href="#/users/listUsers"> <i class="fa fa-users"></i> Users</a>
+  <div class='feed'><div>
   `;
 
 let FeedView = Backbone.View.extend({
   template: _.template(html),
 
-  className: 'feed',
 
   initialize: function() {
     this.listenTo(this.collection, 'add', this.render);
